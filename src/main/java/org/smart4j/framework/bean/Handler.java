129eaf8392fd2191d136d8379ca641b100bb9c9c
lqingfang_279@163.com <lqingfang_279@163.com>
2017-02-12 21:02:21 +0800

    #smart-framework  加载 Controller    request 封装请求信息，路径，方法名    handler 封装处理信息，类，方法    ControllerHelper 中 Map<Request, Handler> 用于存放所有的 request,handler对应关系。    最后调用getHandler(requestMethod,requestPath)  就可以获取handler

---
+++ src/main/java/org/smart4j/framework/bean/Handler.java
@@ -0,0 +1,34 @@
+package org.smart4j.framework.bean;
+
+import java.lang.reflect.Method;
+
+/**
+ * 封装action信息
+ * 处理器信息
+ * 
+ * @author shuang
+ * @version 1.0.0
+ */
+public class Handler {
+	/**
+	 * Controller类
+	 */
+	private Class<?> controllerClass;
+	/**
+	 * Action方法
+	 */
+	private Method actionMethod;
+	
+	public Handler(Class<?> controllerClass, Method actionMethod) {
+		this.controllerClass = controllerClass;
+		this.actionMethod = actionMethod;
+	}
+
+	public Class<?> getControllerClass() {
+		return controllerClass;
+	}
+
+	public Method getActionMethod() {
+		return actionMethod;
+	}
+}
