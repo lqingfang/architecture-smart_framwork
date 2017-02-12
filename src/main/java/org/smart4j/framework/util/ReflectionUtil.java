1eac31fa95082e5a6b652c42eb03c70958b2490a
lqingfang_279@163.com <lqingfang_279@163.com>
2017-02-12 17:03:05 +0800

    #smart-framework  实现bean容器 ClassHelper 获取指定的类集合(getServiceClassSet,getControllerClassSet) ReflectionUtil 封装反射，提供实例化的类 BeanHelper 返回Map<Class<?>, Object>

--- src/main/java/org/smart4j/framework/util/ReflectionUtil.java
+++ src/main/java/org/smart4j/framework/util/ReflectionUtil.java
@@ -1,67 +1,50 @@
 package org.smart4j.framework.util;
 
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
 /**
- * 反射工具类
- * 
- * @author shuang
- * @version 1.0.0
+ * Created by sally on 2017/2/12.
+ * 通过类来实例化对象
  */
 public final class ReflectionUtil {
-	/**
-	 * 创建类的实例
-	 * @param cls Class类的实例
-	 * @return Object 类型的对象
-	 */
-	public static Object newInstance(Class<?> cls) {
-		Object instance = null;
-		try {
-			instance = cls.newInstance();
-		} catch (InstantiationException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
-		return instance;
-	} 
-	/**
-	 * 调用方法
-	 * @param obj Object类的对象
-	 * @param method Method类的对象
-	 * @param args 可变的Object类型的参数
-	 * @return
-	 */
-	public static Object invokeMethod(Object obj, Method method, Object... args) {
-		Object result = null;
-		method.setAccessible(true); // 如果获取到的method是private，不设置为true，就会报错
-		try {
-			method.invoke(obj, args);
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (InvocationTargetException e) {
-			e.printStackTrace();
-		}
-		return result;
-	}
-	/**
-	 * 设置成员变量的值	
-	 * @param obj Object类的对象
-	 * @param field Field类的对象
-	 * @param value Object类的对象
-	 */
-	public static void setField(Object obj, Field field, Object value) {
-		field.setAccessible(true); // 如果获取到的field是private，不设置为true，就会报错
-		try {
-			field.set(obj, value);
-		} catch (IllegalArgumentException e) {
-			e.printStackTrace();
-		} catch (IllegalAccessException e) {
-			e.printStackTrace();
-		}
-	}
+    private static final Logger LOGGER = LoggerFactory.getLogger(ReflectionUtil.class);
+
+    /*
+    创建实例
+     */
+    public static Object newInstance(Class<?> cls) {
+        Object instance;
+        try {
+            instance = cls.newInstance();
+        } catch (Exception e) {
+            LOGGER.error("new instance failure", e);
+            throw new RuntimeException(e);
+        }
+        return instance;
+    }
+    public static Object invokeMethod(Object obj, Method method, Object...args) {
+        Object result;
+        try {
+            method.setAccessible(true);
+            result = method.invoke(obj, args);
+        } catch (Exception e) {
+            LOGGER.error("invoke method failure", e);
+            throw new RuntimeException(e);
+        }
+        return result;
+    }
+    public static void setField(Object obj, Field field, Object value) {
+        try {
+            field.setAccessible(true);
+            field.set(obj, value);
+        } catch (Exception e) {
+            LOGGER.error("set field failure", e);
+            throw new RuntimeException(e);
+        }
+    }
 }
