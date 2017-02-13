2f6b52c868a8ded96c74423e1cb616711926d616
lqingfang_279@163.com <lqingfang_279@163.com>
2017-02-13 11:31:48 +0800

    #smart-framework  初始化框架   理解：没有此处也可以，当第一次访问类时，就会加载static块，有了下面这些代码就可以集中加载了
    第98页以下代码有句话不太明白什么意思。
    
    “实际上，当我们在第一次访问类时，就会加载其static块，这里只是为了让加载更加集中，所以才写了一个HelperLoader类。”
    
    意思是，没有下面这些代码也可以吗？加上下面这些只是为了让加载更加集中吗？
    
    public final class HelperLoader {
    
        public static void init() {
            // 定义需要加载的 Helper 类
            Class<?>[] classList = {
                DatabaseHelper.class,
                EntityHelper.class,
                ActionHelper.class,
                BeanHelper.class,
                AopHelper.class,
                IocHelper.class,
                PluginHelper.class,
            };
            // 按照顺序加载类
            for (Class<?> cls : classList) {
                ClassUtil.loadClass(cls.getName());
            }
        }
    }

--- src/main/java/org/smart4j/framework/HelperLoader.java
+++ src/main/java/org/smart4j/framework/HelperLoader.java
@@ -8,6 +8,8 @@ import org.smart4j.framework.util.ClassUtil;
 
 /**
  * Created by sally on 2017/2/12.
+ * 初始化框架，该处代码只是为了集中加载helper类
+ * （没有此处也可以，当第一次访问类时，就会加载static块）
  */
 public final class HelperLoader {
     public static void init() {
